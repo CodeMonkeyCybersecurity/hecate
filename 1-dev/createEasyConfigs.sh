ad497dbe53f7d8433c8c07eb453c9460820469bb
henry <henry@cybermonkey.net.au>
2025-01-18 15:48:11 +0800

    new file:   1-dev/createEasyConfigs.sh 	modified:   1-dev/docker-compose-test.yaml 	modified:   2-stage/docker-compose.yaml 	modified:   2-stage/nginx.conf

---
+++ 1-dev/createEasyConfigs.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+# createEasyConfigs
+
+mkdir -p "What is the domain/subdomain you want to set up? " DOMAIN
+mkdir -p "What email address can you be contacted at? " MAIL
+
+# TODO: finish
