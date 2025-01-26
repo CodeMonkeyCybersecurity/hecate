1ff53c5f009bc46a4066a4ce9fb05af91e1d28aa
codeMonkeyCybersecurity <henry@cybermonkey.net.au>
2025-01-26 15:11:30 +0800

    Delete 1-dev/createEasyConfigs.sh

--- 1-dev/createEasyConfigs.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-# createEasyConfigs
-
-mkdir -p "What is the domain/subdomain you want to set up? " DOMAIN
-mkdir -p "What email address can you be contacted at? " MAIL
-
-# TODO: finish
