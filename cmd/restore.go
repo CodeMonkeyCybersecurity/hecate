959b6007b428c9b7ca11f1f88e2b59237b33dab0
codeMonkeyCybersecurity <git@cybermonkey.net.au>
2025-03-15 18:34:54 +0800

    Update restore.go

--- cmd/restore.go
+++ cmd/restore.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+Copyright © 2025 NAME HERE
 */
 package cmd
 
@@ -7,80 +7,71 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/spf13/cobra"
 	"github.com/CodeMonkeyCybersecurity/hecate/pkg/utils"
+	"github.com/spf13/cobra"
 )
 
-// restoreCmd represents the restore command
+var timestampFlag string
+
+// restoreCmd represents the restore command.
 var restoreCmd = &cobra.Command{
 	Use:   "restore",
 	Short: "Restore configuration and files from backup",
-	Long:  `Restore the configuration directory, certificates, and docker-compose file from their backups.`,
+	Long: `Restore the configuration directory, certificates, and docker-compose file from their backups.
+
+If --timestamp is provided (e.g. --timestamp 20250325-101010), then restore will look for:
+  conf.d.20250325-101010.bak
+  certs.20250325-101010.bak
+  docker-compose.yml.20250325-101010.bak
+
+If no --timestamp is given, it automatically selects the most recent (lexicographically greatest) backup
+for each of these items.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		runRestore()
 	},
 }
 
 func init() {
+	// Attach restoreCmd to createCmd so that you can run: hecate create restore
 	createCmd.AddCommand(restoreCmd)
+
+	// Define a timestamp flag for optional backup selection.
+	restoreCmd.Flags().StringVarP(&timestampFlag, "timestamp", "t", "",
+		"Timestamp used by backup (format: YYYYMMDD-HHMMSS). If omitted, the most recent backup is used.")
 }
 
 func runRestore() {
 	const (
-		BACKUP_CONF    = "conf.d.bak"
-		BACKUP_CERTS   = "certs.bak"
-		BACKUP_COMPOSE = "docker-compose.yml.bak"
-
-		DST_CONF    = "conf.d"
-		DST_CERTS   = "certs"
-		DST_COMPOSE = "docker-compose.yml"
+		SRC_CONF    = "conf.d"
+		SRC_CERTS   = "certs"
+		SRC_COMPOSE = "docker-compose.yml"
 	)
 
-	// Restore conf.d directory.
-	info, err := os.Stat(BACKUP_CONF)
-	if err != nil || !info.IsDir() {
-		fmt.Printf("Error: Backup directory '%s' does not exist.\n", BACKUP_CONF)
-		os.Exit(1)
-	}
-	if err := utils.RemoveIfExists(DST_CONF); err != nil {
-		fmt.Printf("Error removing %s: %v\n", DST_CONF, err)
-		os.Exit(1)
-	}
-	if err := utils.CopyDir(BACKUP_CONF, DST_CONF); err != nil {
-		fmt.Printf("Error during restore of %s: %v\n", BACKUP_CONF, err)
-		os.Exit(1)
-	}
-	fmt.Printf("Restore complete: '%s' has been restored to '%s'.\n", BACKUP_CONF, DST_CONF)
+	var backupConf, backupCerts, backupCompose string
+	var err error
 
-	// Restore certs directory.
-	info, err = os.Stat(BACKUP_CERTS)
-	if err != nil || !info.IsDir() {
-		fmt.Printf("Error: Backup directory '%s' does not exist.\n", BACKUP_CERTS)
-		os.Exit(1)
+	if timestampFlag != "" {
+		backupConf = fmt.Sprintf("%s.%s.bak", SRC_CONF, timestampFlag)
+		backupCerts = fmt.Sprintf("%s.%s.bak", SRC_CERTS, timestampFlag)
+		backupCompose = fmt.Sprintf("%s.%s.bak", SRC_COMPOSE, timestampFlag)
+	} else {
+		// Automatically detect the most recent backup.
+		if backupConf, err = utils.FindLatestBackup(fmt.Sprintf("%s.", SRC_CONF)); err != nil {
+			fmt.Printf("Error finding latest backup for %s: %v\n", SRC_CONF, err)
+			os.Exit(1)
+		}
+		if backupCerts, err = utils.FindLatestBackup(fmt.Sprintf("%s.", SRC_CERTS)); err != nil {
+			fmt.Printf("Error finding latest backup for %s: %v\n", SRC_CERTS, err)
+			os.Exit(1)
+		}
+		if backupCompose, err = utils.FindLatestBackup(fmt.Sprintf("%s.", SRC_COMPOSE)); err != nil {
+			fmt.Printf("Error finding latest backup for %s: %v\n", SRC_COMPOSE, err)
+			os.Exit(1)
+		}
 	}
-	if err := utils.RemoveIfExists(DST_CERTS); err != nil {
-		fmt.Printf("Error removing %s: %v\n", DST_CERTS, err)
-		os.Exit(1)
-	}
-	if err := utils.CopyDir(BACKUP_CERTS, DST_CERTS); err != nil {
-		fmt.Printf("Error during restore of %s: %v\n", BACKUP_CERTS, err)
-		os.Exit(1)
-	}
-	fmt.Printf("Restore complete: '%s' has been restored to '%s'.\n", BACKUP_CERTS, DST_CERTS)
 
-	// Restore docker-compose.yml file.
-	info, err = os.Stat(BACKUP_COMPOSE)
-	if err != nil || info.IsDir() {
-		fmt.Printf("Error: Backup file '%s' does not exist.\n", BACKUP_COMPOSE)
-		os.Exit(1)
-	}
-	if err := utils.RemoveIfExists(DST_COMPOSE); err != nil {
-		fmt.Printf("Error removing %s: %v\n", DST_COMPOSE, err)
-		os.Exit(1)
-	}
-	if err := utils.CopyFile(BACKUP_COMPOSE, DST_COMPOSE); err != nil {
-		fmt.Printf("Error during restore of %s: %v\n", BACKUP_COMPOSE, err)
-		os.Exit(1)
-	}
-	fmt.Printf("Restore complete: '%s' has been restored to '%s'.\n", BACKUP_COMPOSE, DST_COMPOSE)
+	// Restore the backups.
+	utils.RestoreDir(backupConf, SRC_CONF)
+	utils.RestoreDir(backupCerts, SRC_CERTS)
+	utils.RestoreFile(backupCompose, SRC_COMPOSE)
 }
