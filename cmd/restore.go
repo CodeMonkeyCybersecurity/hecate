642eb04b93d040f03844be5a52b08c50ab07a89c
codeMonkeyCybersecurity <git@cybermonkey.net.au>
2025-03-16 00:29:00 +0800

    Update restore.go

--- cmd/restore.go
+++ cmd/restore.go
@@ -4,8 +4,11 @@ Copyright © 2025 NAME HERE
 package cmd
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/CodeMonkeyCybersecurity/hecate/pkg/utils"
 	"github.com/spf13/cobra"
@@ -20,58 +23,106 @@ var restoreCmd = &cobra.Command{
 	Long: `Restore the configuration directory, certificates, and docker-compose file from their backups.
 
 If --timestamp is provided (e.g. --timestamp 20250325-101010), then restore will look for:
-  conf.d.20250325-101010.bak
-  certs.20250325-101010.bak
-  docker-compose.yml.20250325-101010.bak
+  conf.d.<timestamp>.bak
+  certs.<timestamp>.bak
+  docker-compose.yml.<timestamp>.bak
 
-If no --timestamp is given, it automatically selects the most recent (lexicographically greatest) backup
-for each of these items.`,
+If no --timestamp is given, the command enters interactive mode, where you'll choose which resource(s) to restore.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		runRestore()
+		if timestampFlag != "" {
+			runAutoRestore(timestampFlag)
+		} else {
+			runInteractiveRestore()
+		}
 	},
 }
 
 func init() {
-	// Attach restoreCmd to createCmd so that you can run: hecate create restore
+	// Attach restoreCmd to createCmd so you can run: hecate create restore
 	createCmd.AddCommand(restoreCmd)
 
 	// Define a timestamp flag for optional backup selection.
 	restoreCmd.Flags().StringVarP(&timestampFlag, "timestamp", "t", "",
-		"Timestamp used by backup (format: YYYYMMDD-HHMMSS). If omitted, the most recent backup is used.")
+		"Timestamp used by backup (format: YYYYMMDD-HHMMSS). If omitted, interactive mode is used.")
 }
 
-func runRestore() {
+// runAutoRestore automatically restores resources using the provided timestamp.
+func runAutoRestore(ts string) {
 	const (
 		SRC_CONF    = "conf.d"
 		SRC_CERTS   = "certs"
 		SRC_COMPOSE = "docker-compose.yml"
 	)
 
-	var backupConf, backupCerts, backupCompose string
-	var err error
+	backupConf := fmt.Sprintf("%s.%s.bak", SRC_CONF, ts)
+	backupCerts := fmt.Sprintf("%s.%s.bak", SRC_CERTS, ts)
+	backupCompose := fmt.Sprintf("%s.%s.bak", SRC_COMPOSE, ts)
 
-	if timestampFlag != "" {
-		backupConf = fmt.Sprintf("%s.%s.bak", SRC_CONF, timestampFlag)
-		backupCerts = fmt.Sprintf("%s.%s.bak", SRC_CERTS, timestampFlag)
-		backupCompose = fmt.Sprintf("%s.%s.bak", SRC_COMPOSE, timestampFlag)
-	} else {
-		// Automatically detect the most recent backup.
-		if backupConf, err = utils.FindLatestBackup(fmt.Sprintf("%s.", SRC_CONF)); err != nil {
-			fmt.Printf("Error finding latest backup for %s: %v\n", SRC_CONF, err)
-			os.Exit(1)
-		}
-		if backupCerts, err = utils.FindLatestBackup(fmt.Sprintf("%s.", SRC_CERTS)); err != nil {
-			fmt.Printf("Error finding latest backup for %s: %v\n", SRC_CERTS, err)
-			os.Exit(1)
-		}
-		if backupCompose, err = utils.FindLatestBackup(fmt.Sprintf("%s.", SRC_COMPOSE)); err != nil {
-			fmt.Printf("Error finding latest backup for %s: %v\n", SRC_COMPOSE, err)
-			os.Exit(1)
-		}
-	}
-
-	// Restore the backups.
+	fmt.Printf("Restoring backups with timestamp %s...\n", ts)
 	utils.RestoreDir(backupConf, SRC_CONF)
 	utils.RestoreDir(backupCerts, SRC_CERTS)
+	utils.RestoreFile(backupCompose, SRC_COMPOSE)
+}
+
+// runInteractiveRestore presents a menu to choose which resource(s) to restore.
+func runInteractiveRestore() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("=== Interactive Restore ===")
+	fmt.Println("Select the resource you want to restore:")
+	fmt.Println("1) Restore configuration (conf.d)")
+	fmt.Println("2) Restore certificates (certs)")
+	fmt.Println("3) Restore docker-compose file")
+	fmt.Println("4) Restore all resources")
+	fmt.Print("Enter choice (1-4): ")
+	choice, _ := reader.ReadString('\n')
+	choice = strings.TrimSpace(choice)
+
+	switch choice {
+	case "1":
+		restoreConf()
+	case "2":
+		restoreCerts()
+	case "3":
+		restoreCompose()
+	case "4":
+		restoreConf()
+		restoreCerts()
+		restoreCompose()
+	default:
+		fmt.Println("Invalid choice. Exiting.")
+		os.Exit(1)
+	}
+}
+
+func restoreConf() {
+	const SRC_CONF = "conf.d"
+	backupConf, err := utils.FindLatestBackup(fmt.Sprintf("%s.", SRC_CONF))
+	if err != nil {
+		fmt.Printf("Error finding backup for %s: %v\n", SRC_CONF, err)
+		return
+	}
+	fmt.Printf("Restoring configuration from backup: %s\n", backupConf)
+	utils.RestoreDir(backupConf, SRC_CONF)
+}
+
+func restoreCerts() {
+	const SRC_CERTS = "certs"
+	backupCerts, err := utils.FindLatestBackup(fmt.Sprintf("%s.", SRC_CERTS))
+	if err != nil {
+		fmt.Printf("Error finding backup for %s: %v\n", SRC_CERTS, err)
+		return
+	}
+	fmt.Printf("Restoring certificates from backup: %s\n", backupCerts)
+	utils.RestoreDir(backupCerts, SRC_CERTS)
+}
+
+func restoreCompose() {
+	const SRC_COMPOSE = "docker-compose.yml"
+	backupCompose, err := utils.FindLatestBackup(fmt.Sprintf("%s.", SRC_COMPOSE))
+	if err != nil {
+		fmt.Printf("Error finding backup for %s: %v\n", SRC_COMPOSE, err)
+		return
+	}
+	fmt.Printf("Restoring docker-compose file from backup: %s\n", backupCompose)
 	utils.RestoreFile(backupCompose, SRC_COMPOSE)
 }
