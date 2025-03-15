fcb64d5b66a5fdf2e4b9c3688d4068231ecb8348
codeMonkeyCybersecurity <git@cybermonkey.net.au>
2025-03-15 20:47:32 +0800

    Update compose.go

--- cmd/compose.go
+++ cmd/compose.go
@@ -4,44 +4,15 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"bufio"
 	"fmt"
 	"os"
 	"regexp"
-	"sort"
-	"strconv"
 	"strings"
 
+	"github.com/CodeMonkeyCybersecurity/hecate/pkg/config"
 	"github.com/CodeMonkeyCybersecurity/hecate/pkg/utils"
 	"github.com/spf13/cobra"
 )
-
-// Apps holds all available app options.
-var Apps = []App{
-	{Option: "1", Name: "Static website", ConfFile: "base.conf", Markers: defaultMarkers},
-	{Option: "2", Name: "Wazuh", ConfFile: "delphi.conf", Markers: combineMarkers("1515", "1514", "55000")},
-	{Option: "3", Name: "Mattermost", ConfFile: "collaborate.conf", Markers: defaultMarkers},
-	{Option: "4", Name: "Nextcloud", ConfFile: "cloud.conf", Markers: combineMarkers("3478", "coturn:")},
-	{Option: "5", Name: "Mailcow", ConfFile: "mailcow.conf", Markers: combineMarkers("25", "587", "465", "110", "995", "143", "993")},
-	{Option: "6", Name: "Jenkins", ConfFile: "jenkins.conf", Markers: defaultMarkers},
-	{Option: "7", Name: "Grafana", ConfFile: "observe.conf", Markers: defaultMarkers},
-	{Option: "8", Name: "Umami", ConfFile: "analytics.conf", Markers: defaultMarkers},
-	{Option: "9", Name: "MinIO", ConfFile: "s3.conf", Markers: defaultMarkers},
-	{Option: "10", Name: "Wiki.js", ConfFile: "wiki.conf", Markers: defaultMarkers},
-	{Option: "11", Name: "ERPNext", ConfFile: "erp.conf", Markers: defaultMarkers},
-	{Option: "12", Name: "Jellyfin", ConfFile: "jellyfin.conf", Markers: defaultMarkers},
-	{Option: "13", Name: "Persephone", ConfFile: "persephone.conf", Markers: defaultMarkers},
-}
-
-// getAppByOption returns the App corresponding to a given option string.
-func getAppByOption(option string) (App, bool) {
-	for _, app := range Apps {
-		if app.Option == option {
-			return app, true
-		}
-	}
-	return App{}, false
-}
 
 // composeCmd represents the "compose" subcommand.
 var composeCmd = &cobra.Command{
@@ -86,6 +57,7 @@ If no valid app options are provided, the command will exit with an error.`,
 }
 
 func init() {
+	// Assumes createCmd is defined elsewhere in your application.
 	createCmd.AddCommand(composeCmd)
 }
 
@@ -96,14 +68,14 @@ func runCompose(args []string) {
 		os.Exit(1)
 	}
 
-	var selectedApps map[string]App
+	var selectedApps map[string]config.App
 	var selectionStr string
 
 	// Non-interactive mode: Use provided arguments.
 	if len(args) > 0 {
-		selectedApps = make(map[string]App)
+		selectedApps = make(map[string]config.App)
 		for _, arg := range args {
-			app, ok := getAppByOption(arg)
+			app, ok := config.GetAppByOption(arg)
 			if ok {
 				selectedApps[strings.ToLower(app.Name)] = app
 			}
@@ -119,9 +91,9 @@ func runCompose(args []string) {
 		selectionStr = strings.Join(appsList, ", ")
 	} else {
 		// Interactive mode.
-		displayOptions()
+		config.DisplayOptions()
 		defaultSelection := lastValues["APPS_SELECTION"]
-		selectedApps, selectionStr = getUserSelection(defaultSelection)
+		selectedApps, selectionStr = config.GetUserSelection(defaultSelection)
 		lastValues["APPS_SELECTION"] = selectionStr
 		if err := utils.SaveLastValues(lastValues); err != nil {
 			fmt.Printf("Error saving configuration: %v\n", err)
