3d910c6884d1e10ee76ad150a293814d8fa4593c
codeMonkeyCybersecurity <git@cybermonkey.net.au>
2025-03-15 23:19:18 +0800

    Update config.go

--- cmd/config.go
+++ cmd/config.go
@@ -253,64 +253,67 @@ func runEos() {
 // ------------------ HTTP FUNCTIONALITY ------------------
 //
 
+please help me with this syntax error
+
+
 func runHttp() {
-    configFile := "http.conf"
-    backupFile := "http.conf.bak"
+		configFile := "http.conf"
+		backupFile := "http.conf.bak"
 
-    fmt.Println("Welcome to the HTTP block configuration updater for your http.conf file.")
-    fmt.Println("Below you'll see a description for each setting along with its current default value.")
-    fmt.Println("Press Enter to keep the default value or type a new one as desired.\n")
+		fmt.Println("Welcome to the HTTP block configuration updater for your http.conf file.")
+		fmt.Println("Below you'll see a description for each setting along with its current default value.")
+		fmt.Println("Press Enter to keep the default value or type a new one as desired.\n")
 
-    serverTokens := utils.PromptWithDefault(
-        "Hide NGINX version (server_tokens)",
-        "off",
-        "server_tokens: When set to 'off', NGINX will not display its version in error pages and headers.",
-    )
+		serverTokens := utils.PromptWithDefault(
+			"Hide NGINX version (server_tokens)",
+			"off",
+			"server_tokens: When set to 'off', NGINX will not display its version in error pages and headers.",
+		)
 
-    includeMime := utils.PromptWithDefault(
-        "Path to MIME types file (include)",
-        "mime.types",
-        "include: This file defines MIME types for various file extensions.",
-    )
+		includeMime := utils.PromptWithDefault(
+			"Path to MIME types file (include)",
+			"mime.types",
+			"include: This file defines MIME types for various file extensions.",
+		)
 
-    defaultType := utils.PromptWithDefault(
-        "Default MIME type (default_type)",
-        "application/octet-stream",
-        "default_type: The default MIME type for files with unknown extensions.",
-    )
+		defaultType := utils.PromptWithDefault(
+			"Default MIME type (default_type)",
+			"application/octet-stream",
+			"default_type: The default MIME type for files with unknown extensions.",
+		)
 
-    errorLogPath := utils.PromptWithDefault(
-        "Error log path",
-        "/var/log/nginx/error.log",
-        "error_log: The file path where NGINX will log error messages.",
-    )
+		errorLogPath := utils.PromptWithDefault(
+			"Error log path",
+			"/var/log/nginx/error.log",
+			"error_log: The file path where NGINX will log error messages.",
+		)
 
-    errorLogLevel := utils.PromptWithDefault(
-        "Error log level (e.g., warn, debug)",
-        "warn",
-        "Error log level: Determines the minimum severity of messages to be logged (e.g., 'warn' or 'debug').",
-    )
+		errorLogLevel := utils.PromptWithDefault(
+			"Error log level (e.g., warn, debug)",
+			"warn",
+			"Error log level: Determines the minimum severity of messages to be logged (e.g., 'warn' or 'debug').",
+		)
 
-    accessLogPath := utils.PromptWithDefault(
-        "Access log path",
-        "/var/log/nginx/access.log",
-        "access_log: The file path where NGINX will log access details for incoming requests.",
-    )
+		accessLogPath := utils.PromptWithDefault(
+			"Access log path",
+			"/var/log/nginx/access.log",
+			"access_log: The file path where NGINX will log access details for incoming requests.",
+		)
 
-    sendfile := utils.PromptWithDefault(
-        "Sendfile (on/off)",
-        "on",
-        "sendfile: When 'on', NGINX uses the sendfile system call to transfer files efficiently.",
-    )
+		sendfile := utils.PromptWithDefault(
+			"Sendfile (on/off)",
+			"on",
+			"sendfile: When 'on', NGINX uses the sendfile system call to transfer files efficiently.",
+		)
 
-    includeServers := utils.PromptWithDefault(
-        "Include server blocks directory/pattern",
-        "/etc/nginx/conf.d/servers/*.conf",
-        "include: Specifies the path or pattern for including server block configuration files.",
-    )
+		includeServers := utils.PromptWithDefault(
+			"Include server blocks directory/pattern",
+			"/etc/nginx/conf.d/servers/*.conf",
+			"include: Specifies the path or pattern for including server block configuration files.",
+		)
 
-    // Build the configuration content.
-    configContent := fmt.Sprintf(`###
+		// Build the configuration content.
+		configContent := fmt.Sprintf(`###
 # HTTP BLOCK
 ###
 
@@ -331,33 +334,36 @@ http {
 }
 `, serverTokens, includeMime, defaultType, errorLogPath, errorLogLevel, accessLogPath, sendfile, includeServers)
 
-    // Check if http.conf exists and back it up if necessary.
-    if _, err := os.Stat(configFile); err == nil {
-        if _, err := os.Stat(backupFile); err == nil {
-            if err := os.Remove(backupFile); err != nil {
-                fmt.Printf("Error removing existing backup %s: %v\n", backupFile, err)
-                os.Exit(1)
-            }
-        }
-        if err := os.Rename(configFile, backupFile); err != nil {
-            fmt.Printf("Error backing up %s to %s: %v\n", configFile, backupFile, err)
-            os.Exit(1)
-        }
-        fmt.Printf("\nExisting %s has been backed up to %s\n", configFile, backupFile)
-    }
+		// Check if http.conf exists and back it up if necessary.
+		if _, err := os.Stat(configFile); err == nil {
+			// Remove existing backup if present.
+			if _, err := os.Stat(backupFile); err == nil {
+				if err := os.Remove(backupFile); err != nil {
+					fmt.Printf("Error removing existing backup %s: %v\n", backupFile, err)
+					os.Exit(1)
+				}
+			}
+			// Rename current config file to backup.
+			if err := os.Rename(configFile, backupFile); err != nil {
+				fmt.Printf("Error backing up %s to %s: %v\n", configFile, backupFile, err)
+				os.Exit(1)
+			}
+			fmt.Printf("\nExisting %s has been backed up to %s\n", configFile, backupFile)
+		}
 
-    f, err := os.Create(configFile)
-    if err != nil {
-        fmt.Printf("Error creating %s: %v\n", configFile, err)
-        os.Exit(1)
-    }
-    defer f.Close()
+		// Write the new configuration to http.conf.
+		f, err := os.Create(configFile)
+		if err != nil {
+			fmt.Printf("Error creating %s: %v\n", configFile, err)
+			os.Exit(1)
+		}
+		defer f.Close()
 
-    _, err = f.WriteString(configContent)
-    if err != nil {
-        fmt.Printf("Error writing to %s: %v\n", configFile, err)
-        os.Exit(1)
-    }
+		_, err = f.WriteString(configContent)
+		if err != nil {
+			fmt.Printf("Error writing to %s: %v\n", configFile, err)
+			os.Exit(1)
+		}
 
-    fmt.Printf("\nNew configuration has been written to %s\n", configFile)
-} 
+		fmt.Printf("\nNew configuration has been written to %s\n", configFile)
+}
