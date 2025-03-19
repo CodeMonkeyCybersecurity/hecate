7577445fd7be2f2da6d3d4bd8ba6cf0ba8c26c3d
codeMonkeyCybersecurity <git@cybermonkey.net.au>
2025-03-19 16:49:37 +0800

    Update nginx.go

--- pkg/nginx/nginx.go
+++ pkg/nginx/nginx.go
@@ -1,11 +1,20 @@
+// pkg/nginx/nginx.go
+
 package nginx
 
 import (
 	"fmt"
 	"os/exec"
+	"path/filepath"
 
 	"go.uber.org/zap"
+	"github.com/spf13/cobra"
+	
 	"hecate/pkg/logger"
+	"hecate/pkg/utils"
+	"hecate/pkg/config"
+	"hecate/pkg/docker"
+	
 )
 
 //
@@ -18,20 +27,20 @@ func DeployApp(app string, cmd *cobra.Command) error {
 
 	// Check if the required HTTP config exists
 	httpConfig := filepath.Join(assetsPath, "servers", app+".conf")
-	if !FileExists(httpConfig) {
+	if !utils.FileExists(httpConfig) {
 		logger.Error("Missing HTTP config file", zap.String("file", httpConfig))
 		return fmt.Errorf("missing Nginx HTTP config for %s", app)
 	}
 
 	// Copy HTTP config
-	if err := CopyFile(httpConfig, filepath.Join(nginxConfPath, app+".conf")); err != nil {
+	if err := utils.CopyFile(httpConfig, filepath.Join(nginxConfPath, app+".conf")); err != nil {
 		return fmt.Errorf("failed to copy HTTP config: %w", err)
 	}
 
 	// Copy Stream config if available
 	streamConfig := filepath.Join(assetsPath, "stream", app+".conf")
-	if FileExists(streamConfig) {
-		if err := CopyFile(streamConfig, filepath.Join(nginxStreamPath, app+".conf")); err != nil {
+	if utils.FileExists(streamConfig) {
+		if err := utils.CopyFile(streamConfig, filepath.Join(nginxStreamPath, app+".conf")); err != nil {
 			return fmt.Errorf("failed to copy Stream config: %w", err)
 		}
 	}
@@ -41,7 +50,7 @@ func DeployApp(app string, cmd *cobra.Command) error {
 		noTalk, _ := cmd.Flags().GetBool("without-talk")
 		if !noTalk {
 			logger.Info("Deploying Coturn for NextCloud Talk")
-			if err := RunDockerComposeService(dockerComposeFile, "coturn"); err != nil {
+			if err := docker.RunDockerComposeService(dockerComposeFile, "coturn"); err != nil {
 				return fmt.Errorf("failed to deploy Coturn: %w", err)
 			}
 		} else {
