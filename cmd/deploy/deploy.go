86dfd3e2a62bb26c4c1db4c8a46edb2c031eba1e
codeMonkeyCybersecurity <git@cybermonkey.net.au>
2025-03-19 15:09:39 +0800

    Update deploy.go

--- cmd/deploy/deploy.go
+++ cmd/deploy/deploy.go
@@ -30,6 +30,6 @@ func runDeploy(cmd *cobra.Command, args []string) {
 }
 
 func init() {
-	rootCmd.AddCommand(DeployCmd)
+	rootCmd.AddCommand(deployCmd)
 	DeployCmd.Flags().Bool("without-talk", false, "Deploy NextCloud without Coturn")
 }
