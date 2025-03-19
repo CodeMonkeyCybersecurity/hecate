fad418fa1a384e581eb9144938f9f63d971f40fb
codeMonkeyCybersecurity <git@cybermonkey.net.au>
2025-03-19 14:51:05 +0800

    Update deploy.go

--- cmd/deploy/deploy.go
+++ cmd/deploy/deploy.go
@@ -2,12 +2,8 @@ package deploy
 
 import (
 	"fmt"
-	"os"
-	"path/filepath"
 
-	"hecate/pkg/utils" // Ensure this exists
-	"hecate/pkg/config"
-
+	"hecate/pkg/utils"
 	"github.com/spf13/cobra"
 	"go.uber.org/zap"
 )
@@ -16,16 +12,24 @@ import (
 var DeployCmd = &cobra.Command{
 	Use:   "deploy [app]",
 	Short: "Deploy an application behind the Hecate reverse proxy",
-	Long: `Deploys an Nginx reverse proxy configuration for a specified application.
+	Args:  cobra.ExactArgs(1),
+	Run:   runDeploy,
+}
 
-Example:
-  hecate deploy nextcloud
-  hecate deploy wazuh
-  hecate deploy jenkins`,
-	Args: cobra.ExactArgs(1), // Ensure exactly one argument is provided
-	Run:  runDeploy,
+func runDeploy(cmd *cobra.Command, args []string) {
+	app := args[0]
+	logger := utils.GetLogger()
+
+	logger.Info("Starting deployment", zap.String("app", app))
+
+	// Deploy the application using utils
+	if err := utils.DeployApp(app, cmd); err != nil {
+		logger.Error("Deployment failed", zap.Error(err))
+		fmt.Println("Error:", err)
+	}
 }
 
 func init() {
 	rootCmd.AddCommand(DeployCmd)
+	DeployCmd.Flags().Bool("without-talk", false, "Deploy NextCloud without Coturn")
 }
