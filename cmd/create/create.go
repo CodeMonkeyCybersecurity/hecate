3797fc44279f84f6a12eefa5d1db060b593b74d0
codeMonkeyCybersecurity <git@cybermonkey.net.au>
2025-03-24 03:37:52 +0800

    Update create.go

--- cmd/create/create.go
+++ cmd/create/create.go
@@ -1,18 +1,29 @@
 // cmd/create/create.go
-
 package create
 
 import (
-	"github.com/spf13/cobra"
+    "github.com/spf13/cobra"
+    "go.uber.org/zap"
+
+    "hecate/cmd/create/hetzner"
+    "hecate/pkg/logger"
 )
+
+var log = logger.GetLogger()
 
 // CreateCmd represents the create command
 var CreateCmd = &cobra.Command{
-	Use:   "create",
-	Short: "Create resources for Hecate",
-	Long: `The create command allows you to create specific resources
+    Use:   "create",
+    Short: "Create resources for Hecate",
+    Long: `The create command allows you to create specific resources
 needed for your Hecate deployment, such as certificates, proxy configurations, etc.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		cmd.Println("Create command executed!")
-	},
+    Run: func(cmd *cobra.Command, args []string) {
+        cmd.Println("Create command executed!")
+    },
+}
+
+// init gets called automatically at package load time
+func init() {
+    // Attach the hetzner-wildcard subcommand here
+    CreateCmd.AddCommand(hetzner.NewCreateHetznerWildcardCmd())
 }
